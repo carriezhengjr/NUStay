770202944e1abc76a135e9fa8bbcf96926307c35
stanleytangzh <111631172+stanleytangzh@users.noreply.github.com>
2024-06-28 01:41:35 +0800

    Removed unwanted files

--- src/firebase/updateUserRecords.js
+++
@@ -1,9 +0,0 @@
-import { doc, setDoc } from 'firebase/firestore';
-import { db } from './firebase';
-
-const updateUserRecords = (collectionName, uid, updatedObj) => {
-  const userDocRef = doc(db, collectionName, uid);
-  return setDoc(userDocRef, updatedObj, { merge: true });
-};
-
-export default updateUserRecords;
